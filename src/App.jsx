b82c7effe3b4750903a9f4187cd31b8dbc831985
zihad868 <zihadh868@gmail.com>
2024-07-03 12:04:01 +0600

    Portfolio Design

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Button</button>
+    </>
+  );
+}
+
+export default App;
