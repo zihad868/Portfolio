874cfe07ff73854a850cd4c3a67a83916becdfab
zihad868 <zihadh868@gmail.com>
2024-07-04 13:39:13 +0600

    Banner Section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,45 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   const activeNavLink = ({ isActive }) => (isActive ? "text-primary" : "");
 
   const navLinks = (
     <>
-      <NavLink to="/" className={activeNavLink}>
-        Home
-      </NavLink>
-      <NavLink to="/about" className={activeNavLink}>
-        About
-      </NavLink>
-      <NavLink to="/skill" className={activeNavLink}>
-        Skill
-      </NavLink>
-      <NavLink to="/contact" className={activeNavLink}>
-        Contact me
-      </NavLink>
+        <NavLink to="/" className={activeNavLink}>
+          Home
+        </NavLink>
+
+        <NavLink to="/about" className={activeNavLink}>
+          About
+        </NavLink>
+
+        <NavLink to="/skill" className={activeNavLink}>
+          Skill
+        </NavLink>
+
+        <NavLink to="/contact" className={activeNavLink}>
+          Contact me
+        </NavLink>
+
     </>
   );
 
   return (
-    <div>
-      <div className="navbar w-full fixed z-30">
+    <div className="fixed top-0 left-0 right-0 z-30">
+      <div className="navbar w-11/12 mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
-            <div
+            <button
               tabIndex={0}
-              role="button"
               className="btn btn-ghost md:hidden lg:hidden"
+              onClick={toggleDropdown}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -44,15 +55,17 @@ const Navbar = () => {
                   d="M4 6h16M4 12h8m-8 6h16"
                 />
               </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              {navLinks}
-            </ul>
+            </button>
+            {dropdownOpen && (
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow"
+              >
+                {navLinks}
+              </ul>
+            )}
           </div>
-          <a className="md:text-xl lg:text-2xl">Zihad</a>
+          <a className="md:text-xl lg:text-2x font-bold">Zihad</a>
         </div>
 
         <div className="navbar-end hidden md:flex lg:flex">
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Home.css';
-
+import profile from '../../assets/Profile/zihad.png';
 
 const Home = () => {
     return (
         <div className='cover'>
             <br /><br /> <br />
-            Home Page
+            <div className='w-11/12 mx-auto min-h-72 lg:grid grid-cols-2 justify-center items-center'>
+                <div>
+                    <h3 className='text-4xl font-bold'>Hi! I am Md. Zihad</h3>
+                    <h2 className='test-2xl'>frontend developer</h2>
+                </div>
+                <div>
+                    <img src={profile} alt="Zihad Image" />
+                </div>
+            </div>
         </div>
     );
 };
